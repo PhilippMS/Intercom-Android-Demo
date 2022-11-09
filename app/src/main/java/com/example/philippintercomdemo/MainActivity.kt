673c5713928557f4701725d3786409de1f2a7aa0
PhilippMS <philipp.speidel.work@gmail.com>
2022-11-09 12:07:29 +0000

    Update to new Intercom SDK (14.0.0) Added new Intercom features (Event Submit Button & Survey)

--- app/src/main/java/com/example/philippintercomdemo/MainActivity.kt
+++ app/src/main/java/com/example/philippintercomdemo/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.example.philippintercomdemo
 
-import android.app.Application
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 //Imported based on Intercom.initialize
-import io.intercom.android.sdk.Intercom
 //Imported based on Registration.create()
-import io.intercom.android.sdk.identity.Registration
 //Imported based on Button Usage
-import android.widget.Button
 //Imported based on SwitchMaterial Usage / alternatively you could use SwitchCompat / or outdated Switch
 //import android.widget.Switch
+
+//import android.R
+import android.app.Application
+import android.os.Bundle
+//import android.view.View
+import android.widget.Button
+import android.widget.TextView
+//import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.switchmaterial.SwitchMaterial
+import io.intercom.android.sdk.Intercom
+import io.intercom.android.sdk.IntercomContent
+import io.intercom.android.sdk.identity.Registration
+import java.time.Instant
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+//import java.util.TimeZone
 
 
 //Intercom (Custom Class to initialize Intercom)
@@ -29,8 +39,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //Intercom (Registration):
-        val registration = Registration.create().withEmail(getString(R.string.intercom_user_email))
-        Intercom.client().registerIdentifiedUser(registration)
+        //val registration = Registration.create().withEmail(getString(R.string.intercom_user_email))
+        //Intercom.client().registerIdentifiedUser(registration)
+
+        //Intercom (Registration):
+        val userRegistration = Registration.create().withUserId(getString(R.string.intercom_user_id))
+        Intercom.client().loginIdentifiedUser(userRegistration)
+
+        //Custom (Messenger Counter)
+        val messageCounter = findViewById<TextView>(R.id.unreadCounter)
+        Intercom.client().addUnreadConversationCountListener {
+            messageCounter.text = "$it"
+        }
 
         //Custom (Messenger Button)
         // get reference to button
@@ -38,13 +58,44 @@ class MainActivity : AppCompatActivity() {
         // set on-click listener
         buttonMessenger.setOnClickListener {
             // your code to perform when the user clicks on the button
-            Intercom.client().displayMessenger()
+            Intercom.client().present()
         }
 
         //Custom (Carousel Button)
         val buttonCarousel = findViewById<Button>(R.id.buttonCarousel)
         buttonCarousel.setOnClickListener {
-            Intercom.client().displayCarousel(getString(R.string.intercom_carousel_id))
+            Intercom.client().presentContent(IntercomContent.Carousel(getString(R.string.intercom_carousel_id)))
+        }
+
+        //Custom (Survey Button)
+        val buttonSurvey = findViewById<Button>(R.id.buttonSurvey)
+        buttonSurvey.setOnClickListener {
+            Intercom.client().presentContent(IntercomContent.Survey(getString(R.string.intercom_survey_id)))
+        }
+
+        //Custom (Event Button)
+        val buttonEvent = findViewById<Button>(R.id.buttonEvent)
+        buttonEvent.setOnClickListener {
+
+            //Formatting Meta Data
+            val currentDate = DateTimeFormatter
+                .ofPattern("yyyy-MM-dd")
+                .withZone(ZoneOffset.UTC)
+                .format(Instant.now())
+            val currentTime = DateTimeFormatter
+                .ofPattern("HH:mm:ss")
+                .withZone(ZoneOffset.UTC)
+                .format(Instant.now())
+            val triggerId = (1000..9999).random()
+
+            //Creating event with Metadata
+            val eventData : Map<String, *> = mapOf(
+                "trigger_date" to currentDate,
+                "trigger_time" to currentTime,
+                "event_id" to triggerId
+            )
+            //Sending Event
+            Intercom.client().logEvent("mobile_demo_event", eventData)
         }
 
         //Custom (Launcher Switch)
